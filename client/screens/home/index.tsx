a6eccc4f0ee7fa6da5d7b65db999ba9fb27e85e8
user5319619790 <749235805105097-user5319619790@noreply.coze.cn>
2026-04-20 12:07:39 +0800

    feat: logo 往左移

--- client/screens/home/index.tsx
+++ client/screens/home/index.tsx
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 50,
-    marginRight: 0,
+    marginLeft: 20,
   },
   logoImage: {
     width: 180,
