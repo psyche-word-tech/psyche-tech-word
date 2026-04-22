07b026dc410cda67ce07a9e7d0f55d68b6a1d667
user5319619790 <749235805105097-user5319619790@noreply.coze.cn>
2026-04-22 23:19:55 +0800

    style: 购买词汇书图标放大到140px

--- client/screens/study/index.tsx
+++ client/screens/study/index.tsx
@@ -151,8 +151,8 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   dangImage: {
-    width: '100%',
-    height: '100%',
+    width: 140,
+    height: 140,
   },
   gridLabel: {
     fontSize: 14,
