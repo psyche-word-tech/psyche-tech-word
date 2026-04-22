29bbb87dbeac92f530772e4fbbd8e097d3fd259b
user5319619790 <749235805105097-user5319619790@noreply.coze.cn>
2026-04-23 00:11:23 +0800

    fix: 启动页logo宽度从200改为100

--- client/app.config.ts
+++ client/app.config.ts
@@ -44,7 +44,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         "expo-splash-screen",
         {
           "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
+          "imageWidth": 100,
           "resizeMode": "contain",
           "backgroundColor": "#ffffff"
         }
