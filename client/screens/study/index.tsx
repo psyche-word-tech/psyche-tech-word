38c94e17a17914a7ef1e9081f9666bf65fde7f73
user5319619790 <749235805105097-user5319619790@noreply.coze.cn>
2026-04-23 21:44:35 +0800

    feat: 区域a图片铺满布局

--- client/screens/study/index.tsx
+++ client/screens/study/index.tsx
@@ -144,6 +144,11 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   regionAStyle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     width: '100%',
     height: '100%',
   },
