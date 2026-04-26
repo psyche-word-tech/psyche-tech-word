f8191f6f7ec5a20866fb37a2c5859a110c7139e7
user5319619790 <749235805105097-user5319619790@noreply.coze.cn>
2026-04-26 12:52:59 +0800

    style: 缩小每日单词界面单词字号

--- client/screens/word-detail/index.tsx
+++ client/screens/word-detail/index.tsx
@@ -705,7 +705,7 @@ const styles = StyleSheet.create({
 		marginLeft: 40,
 	},
 	wordText: {
-		fontSize: 36,
+		fontSize: 28,
 		fontWeight: '700',
 		color: '#333333',
 		fontFamily: 'Times New Roman',
