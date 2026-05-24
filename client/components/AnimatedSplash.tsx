24599b5416c3a8694d2213ddbfc5e17ed492a499
user6080370808 <995532409411064-user6080370808@noreply.coze.cn>
2026-05-24 10:11:01 +0800

    fix: 改用整体缩放动画确保原图完整
    Coze-Commit-Type: user
    Coze-User-ID: 995532409411064
    Coze-Conversation-ID: 22804994747650

--- client/components/AnimatedSplash.tsx
+++ client/components/AnimatedSplash.tsx
@@ -6,163 +6,52 @@ interface AnimatedSplashProps {
 }
 
 export default function AnimatedSplash({ onAnimationComplete }: AnimatedSplashProps) {
-  const topLeftAnim = useRef(new Animated.Value(0)).current;
-  const topRightAnim = useRef(new Animated.Value(0)).current;
-  const bottomLeftAnim = useRef(new Animated.Value(0)).current;
-  const bottomRightAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.3)).current;
+  const opacityAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    const animations = Animated.sequence([
-      Animated.timing(topLeftAnim, {
+    // 整体缩放+淡入动画
+    Animated.parallel([
+      Animated.timing(scaleAnim, {
         toValue: 1,
-        duration: 400,
-        easing: Easing.out(Easing.cubic),
+        duration: 800,
+        easing: Easing.out(Easing.back(1.5)),
         useNativeDriver: true,
       }),
-      Animated.parallel([
-        Animated.timing(topRightAnim, {
-          toValue: 1,
-          duration: 400,
-          easing: Easing.out(Easing.cubic),
-          useNativeDriver: true,
-        }),
-      ]),
-      Animated.parallel([
-        Animated.timing(bottomLeftAnim, {
-          toValue: 1,
-          duration: 400,
-          easing: Easing.out(Easing.cubic),
-          useNativeDriver: true,
-        }),
-      ]),
-      Animated.parallel([
-        Animated.timing(bottomRightAnim, {
-          toValue: 1,
-          duration: 400,
-          easing: Easing.out(Easing.cubic),
-          useNativeDriver: true,
-        }),
-      ]),
-    ]);
-
-    animations.start(() => {
+      Animated.timing(opacityAnim, {
+        toValue: 1,
+        duration: 400,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
       setTimeout(() => {
         onAnimationComplete?.();
-      }, 1000);
+      }, 1200);
     });
 
     return () => {
-      topLeftAnim.stopAnimation();
-      topRightAnim.stopAnimation();
-      bottomLeftAnim.stopAnimation();
-      bottomRightAnim.stopAnimation();
+      scaleAnim.stopAnimation();
+      opacityAnim.stopAnimation();
     };
   }, []);
 
-  // 原图尺寸 400x332，容器 300x249 (保持比例)
-  // 每个象限的大小
-  const quadrantW = 150;
-  const quadrantH = 124.5;
-
   return (
     <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        {/* 左上角 */}
-        <Animated.View
-          style={[
-            styles.quadrant,
-            {
-              left: 0,
-              top: 0,
-              opacity: topLeftAnim,
-              transform: [
-                { translateX: topLeftAnim.interpolate({ inputRange: [0, 1], outputRange: [-300, 0] }) },
-                { translateY: topLeftAnim.interpolate({ inputRange: [0, 1], outputRange: [-250, 0] }) },
-              ],
-            },
-          ]}
-        >
-          <View style={styles.clipContainer}>
-            <Image
-              source={require('@/assets/splash-logo.png')}
-              style={[styles.fullImage, { left: 0, top: 0 }]}
-              resizeMode="contain"
-            />
-          </View>
-        </Animated.View>
-
-        {/* 右上角 */}
-        <Animated.View
-          style={[
-            styles.quadrant,
-            {
-              right: 0,
-              top: 0,
-              opacity: topRightAnim,
-              transform: [
-                { translateX: topRightAnim.interpolate({ inputRange: [0, 1], outputRange: [300, 0] }) },
-                { translateY: topRightAnim.interpolate({ inputRange: [0, 1], outputRange: [-250, 0] }) },
-              ],
-            },
-          ]}
-        >
-          <View style={styles.clipContainer}>
-            <Image
-              source={require('@/assets/splash-logo.png')}
-              style={[styles.fullImage, { right: 0, top: 0, left: 'auto' }]}
-              resizeMode="contain"
-            />
-          </View>
-        </Animated.View>
-
-        {/* 左下角 */}
-        <Animated.View
-          style={[
-            styles.quadrant,
-            {
-              left: 0,
-              bottom: 0,
-              opacity: bottomLeftAnim,
-              transform: [
-                { translateX: bottomLeftAnim.interpolate({ inputRange: [0, 1], outputRange: [-300, 0] }) },
-                { translateY: bottomLeftAnim.interpolate({ inputRange: [0, 1], outputRange: [250, 0] }) },
-              ],
-            },
-          ]}
-        >
-          <View style={styles.clipContainer}>
-            <Image
-              source={require('@/assets/splash-logo.png')}
-              style={[styles.fullImage, { left: 0, bottom: 0, top: 'auto' }]}
-              resizeMode="contain"
-            />
-          </View>
-        </Animated.View>
-
-        {/* 右下角 */}
-        <Animated.View
-          style={[
-            styles.quadrant,
-            {
-              right: 0,
-              bottom: 0,
-              opacity: bottomRightAnim,
-              transform: [
-                { translateX: bottomRightAnim.interpolate({ inputRange: [0, 1], outputRange: [300, 0] }) },
-                { translateY: bottomRightAnim.interpolate({ inputRange: [0, 1], outputRange: [250, 0] }) },
-              ],
-            },
-          ]}
-        >
-          <View style={styles.clipContainer}>
-            <Image
-              source={require('@/assets/splash-logo.png')}
-              style={[styles.fullImage, { right: 0, bottom: 0, left: 'auto', top: 'auto' }]}
-              resizeMode="contain"
-            />
-          </View>
-        </Animated.View>
-      </View>
+      <Animated.View
+        style={[
+          styles.logoWrapper,
+          {
+            transform: [{ scale: scaleAnim }],
+            opacity: opacityAnim,
+          },
+        ]}
+      >
+        <Image
+          source={require('@/assets/splash-logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </Animated.View>
     </View>
   );
 }
@@ -175,25 +64,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     zIndex: 999,
   },
-  logoContainer: {
+  logoWrapper: {
     width: 300,
     height: 249,
-    position: 'relative',
   },
-  quadrant: {
-    position: 'absolute',
-    width: 150,
-    height: 124.5,
-    overflow: 'hidden',
-  },
-  clipContainer: {
-    width: 150,
-    height: 124.5,
-    overflow: 'hidden',
-  },
-  fullImage: {
-    width: 300,
-    height: 249,
-    position: 'absolute',
+  logo: {
+    width: '100%',
+    height: '100%',
   },
 });
